79b76e126e6fc3a0e17c393c3a273923f6460feb
marcospedroche05 <marcospedroche62@gmail.com>
2026-01-14 09:26:28 +0100

    eliminar enfermo por su inscripcion

---
+++ NetCoreAdoNet/Form3EliminarEnfermo.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form3EliminarEnfermo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtInscripcion = new TextBox();
+            btnEliminar = new Button();
+            label2 = new Label();
+            lstEnfermos = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Inscripcion";
+            // 
+            // txtInscripcion
+            // 
+            txtInscripcion.Location = new Point(54, 83);
+            txtInscripcion.Name = "txtInscripcion";
+            txtInscripcion.Size = new Size(120, 23);
+            txtInscripcion.TabIndex = 1;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(54, 122);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(100, 45);
+            btnEliminar.TabIndex = 2;
+            btnEliminar.Text = "Eliminar enfermo";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(212, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Enfermos";
+            // 
+            // lstEnfermos
+            // 
+            lstEnfermos.FormattingEnabled = true;
+            lstEnfermos.Location = new Point(212, 83);
+            lstEnfermos.Name = "lstEnfermos";
+            lstEnfermos.Size = new Size(172, 214);
+            lstEnfermos.TabIndex = 4;
+            // 
+            // Form3EliminarEnfermo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(439, 372);
+            Controls.Add(lstEnfermos);
+            Controls.Add(label2);
+            Controls.Add(btnEliminar);
+            Controls.Add(txtInscripcion);
+            Controls.Add(label1);
+            Name = "Form3EliminarEnfermo";
+            Text = "Form3EliminarEnfermo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtInscripcion;
+        private Button btnEliminar;
+        private Label label2;
+        private ListBox lstEnfermos;
+    }
+}
---
+++ NetCoreAdoNet/Form3EliminarEnfermo.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form3EliminarEnfermo : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public Form3EliminarEnfermo()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.LoadEnfermos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //NECESITAMOS EL DATO DE INSCRIPCION CONCATENADO
+            string inscripcion = this.txtInscripcion.Text;
+            string sql = "DELETE FROM ENFERMO WHERE INSCRIPCION = " + inscripcion;
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            //LAS CONSULTAS DE ACCION DEVUELVEN UN INT CON EL NUMERO DE REGISTROS AFECTADOS
+            int registros = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.LoadEnfermos();
+            MessageBox.Show("Enfermos eliminados: " + registros);
+        }
+
+        private void LoadEnfermos()
+        {
+            string sql = "select * from ENFERMO";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.lstEnfermos.Items.Clear();
+            while (this.reader.Read())
+            {
+                string inscripcion = this.reader["INSCRIPCION"].ToString();
+                string apellido = this.reader["APELLIDO"].ToString();
+                this.lstEnfermos.Items.Add(inscripcion + " - " + apellido);
+            }
+            this.reader.Close();
+            this.cn.Close();
+        }
+    }
+}
