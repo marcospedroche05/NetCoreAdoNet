5f01c48d5288956076c9d1dc6a085762ef6e58ac
marcospedroche05 <marcospedroche62@gmail.com>
2026-01-14 10:49:11 +0100

    update salas por nombre

---
+++ NetCoreAdoNet/Form05UpdateSalas.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form05UpdateSalas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstSalas = new ListBox();
+            label2 = new Label();
+            txtNombre = new TextBox();
+            btnUpdate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Salas";
+            // 
+            // lstSalas
+            // 
+            lstSalas.FormattingEnabled = true;
+            lstSalas.Location = new Point(44, 77);
+            lstSalas.Name = "lstSalas";
+            lstSalas.Size = new Size(173, 214);
+            lstSalas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(239, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nuevo nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(239, 77);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 23);
+            txtNombre.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(239, 128);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(125, 31);
+            btnUpdate.TabIndex = 4;
+            btnUpdate.Text = "Update salas";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // Form05UpdateSalas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(413, 374);
+            Controls.Add(btnUpdate);
+            Controls.Add(txtNombre);
+            Controls.Add(label2);
+            Controls.Add(lstSalas);
+            Controls.Add(label1);
+            Name = "Form05UpdateSalas";
+            Text = "Form05UpdateSalas";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstSalas;
+        private Label label2;
+        private TextBox txtNombre;
+        private Button btnUpdate;
+    }
+}
---
+++ NetCoreAdoNet/Form05UpdateSalas.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form05UpdateSalas : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public Form05UpdateSalas()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.LoadSalas();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string sql = "UPDATE SALA SET NOMBRE=@newname where NOMBRE = @oldname";
+            string newName = this.txtNombre.Text;
+            string oldName = this.lstSalas.SelectedItem.ToString();
+            SqlParameter pamNew = new SqlParameter("@newname", newName);
+            SqlParameter pamOld = new SqlParameter("@oldname", oldName);
+            this.com.Parameters.Add(pamNew);
+            this.com.Parameters.Add(pamOld);
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int registros = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            MessageBox.Show("Salas modificadas: " + registros);
+            this.LoadSalas();
+        }
+
+        private void LoadSalas()
+        {
+            string sql = "SELECT DISTINCT NOMBRE FROM SALA";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.lstSalas.Items.Clear();
+            while (this.reader.Read())
+            {
+                string nombreSala = this.reader["NOMBRE"].ToString();
+                this.lstSalas.Items.Add(nombreSala);
+            }
+            this.reader.Close();
+            this.cn.Close();
+        }
+    }
+}
