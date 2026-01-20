77987637f63911bd38020af16de91f4c31f99665
marcospedroche05 <marcospedroche62@gmail.com>
2026-01-20 10:37:01 +0100

    Form 14 pruebas con settingsJson

--- NetCoreAdoNet/Form14Settings.Designer.cs
+++ NetCoreAdoNet/Form14Settings.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form14Settings";
+            btnLeerSettings = new Button();
+            lblConexion = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // btnLeerSettings
+            // 
+            btnLeerSettings.Location = new Point(35, 35);
+            btnLeerSettings.Name = "btnLeerSettings";
+            btnLeerSettings.Size = new Size(155, 44);
+            btnLeerSettings.TabIndex = 0;
+            btnLeerSettings.Text = "Leer Settings";
+            btnLeerSettings.UseVisualStyleBackColor = true;
+            btnLeerSettings.Click += btnLeerSettings_Click;
+            // 
+            // lblConexion
+            // 
+            lblConexion.AutoSize = true;
+            lblConexion.Location = new Point(35, 103);
+            lblConexion.Name = "lblConexion";
+            lblConexion.Size = new Size(71, 15);
+            lblConexion.TabIndex = 1;
+            lblConexion.Text = "lblConexion";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(35, 155);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(155, 163);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(237, 155);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(158, 163);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // Form14Settings
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 386);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblConexion);
+            Controls.Add(btnLeerSettings);
+            Name = "Form14Settings";
+            Text = "Form14Settings";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnLeerSettings;
+        private Label lblConexion;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- NetCoreAdoNet/Form14Settings.cs
+++ NetCoreAdoNet/Form14Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,34 @@ namespace NetCoreAdoNet
         public Form14Settings()
         {
             InitializeComponent();
+        }
+
+        private void btnLeerSettings_Click(object sender, EventArgs e)
+        {
+            //NECESITO UN CONSTRUCTOR DE CONFIGURACIONES 
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            //EN ESTE ENTORNO DE PROYECTO, SETTINGS NO ES NATIVO, ES DECIR
+            //A PESAR DE LLAMARLO appsettings.json, NO LO RECONOCE
+            //DEBEMOS INDICAR LA UBICACION DEL FICHERO Y EL NOMBRE
+            builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json", false, true);
+            //NECESITAMOS EL OBJETO PARA RECUPERAR LAS KEYS
+            IConfigurationRoot configuration = builder.Build();
+            //EXISTEN KEYS YA CONFIGURADAS Y PODEMOS RECUPERARLAS CON METODOS NATIVOS
+            //LAS KEYS DIFERENCIAN MAYUSCULAS DE MINUSCULAS
+            string connectionString = configuration.GetConnectionString("SqlLocalTajamar");
+            this.lblConexion.Text = connectionString;
+            //SI NO SON KEYS CONOCIDAS, DEBEMOS NAVEGAR HASTA ELLAS
+            //LA NAVEGACION ENTRE KEYS SE ESTABLECE MEDIANTE :
+            //KeyPrincipal:Subkey:OtraSubkey
+            string imagen1 = configuration.GetSection("Imagenes:imagen1").Value;
+            string imagen2 = configuration.GetSection("Imagenes:imagen2").Value;
+            string colorLetra = configuration.GetSection("Colores:letra").Value;
+            string colorFondo = configuration.GetSection("Colores:fondo").Value;
+            this.pictureBox1.Load(imagen1);
+            this.pictureBox2.Load(imagen2);
+            this.BackColor = Color.FromName(colorFondo);
+            this.btnLeerSettings.ForeColor = Color.FromName(colorLetra);
+            
         }
     }
 }
